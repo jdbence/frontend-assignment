79605966f1ab2efa624a9b0217f22d8717928ba2
santhu-swetha <swetha.kendyala@gail.com>
2017-02-25 10:09:36 -0800

    indentation

--- src/components/Grid/MarkerWindowLeft.js
+++ src/components/Grid/MarkerWindowLeft.js
@@ -5,75 +5,76 @@ import d3 from "d3"
 import classes from './Grid.scss'
 
 class MarkerWindowLeft extends React.Component {
- render() {
-  this.state = {
-   markerCoords: randomDataSet()
-  };
-  var drag = (ev)=>{
-   ev.dataTransfer.setData("markerId", ev.target.id);
-   ev.dataTransfer.setData("backgroundColor", colors[parseInt(ev.target.id.substr(-1,1))]);
-  };
-  return (
-   <div className={classes.markerWindow}>
+  render() {
+    this.state = {
+      markerCoords: randomDataSet()
+    };
+    var drag = (ev)=>{
+      ev.dataTransfer.setData("markerId", ev.target.id);
+      ev.dataTransfer.setData("backgroundColor", colors[parseInt(ev.target.id.substr(-1,1))]);
+    };
+    return (
+      <div className={classes.markerWindow}>
 
-   < div className={[classes.markerRow,classes.header].join(" ")} >
-   <span> X </span>
-    <span> Y </span>
-    <span> Marker </span>
-   </div>
+        < div className={[classes.markerRow,classes.header].join(" ")} >
+          <span> X </span>
+          <span> Y </span>
+          <span> Marker </span>
+        </div>
 
-  < div className={classes.markerRow} >
-  <span>{this.state.markerCoords[0][1]} </span>
-  <span> {this.state.markerCoords[0][0]} < /span>
-  < span className={[classes.marker, classes.marker0].join(" ")}
-   id = "marker-0"
-   data-x = {this.state.markerCoords[0][1]}
-   data-y = {this.state.markerCoords[0][0]}
-   draggable = "true"
-   onDragStart = {drag}
-   onClick ={drag} >
-   < /span>
-   < /div>
+      < div className={classes.markerRow} >
+        <span>{this.state.markerCoords[0][1]} </span>
+        <span> {this.state.markerCoords[0][0]} < /span>
+        < span className={[classes.marker, classes.marker0].join(" ")}
+          id = "marker-0"
+          data-x = {this.state.markerCoords[0][1]}
+          data-y = {this.state.markerCoords[0][0]}
+          draggable = "true"
+          onDragStart = {drag}
+          onClick ={drag} >
+        < /span>
+      < /div>
 
-   < div  className={classes.markerRow}  >
-   <span>{this.state.markerCoords[1][1]} </span>
-   <span> {this.state.markerCoords[1][0]} < /span>
-   < span className={[classes.marker, classes.marker1].join(" ")}
-   id = "marker-1"
-   data-x = {this.state.markerCoords[1][1]}
-   data-y = {this.state.markerCoords[1][0]}
-   draggable = "true"
-   onDragStart = {drag}
-   onClick ={drag} >
-   < /span>
-   < /div>
+      < div  className={classes.markerRow}  >
+        <span>{this.state.markerCoords[1][1]} </span>
+        <span> {this.state.markerCoords[1][0]} < /span>
+        < span className={[classes.marker, classes.marker1].join(" ")}
+          id = "marker-1"
+          data-x = {this.state.markerCoords[1][1]}
+          data-y = {this.state.markerCoords[1][0]}
+          draggable = "true"
+          onDragStart = {drag}
+          onClick ={drag} >
+        < /span>
+      < /div>
 
-   < div  className={classes.markerRow} >
-   <span>{this.state.markerCoords[2][1]} </span>
-   <span> {this.state.markerCoords[2][0]} < /span>
-   < span className={[classes.marker, classes.marker2].join(" ")}
-   id = "marker-2"
-   data-x = {this.state.markerCoords[2][1]}
-   data-y = {this.state.markerCoords[2][0]}
-   draggable = "true"
-   onDragStart = {drag} >
-   < /span>
-   < /div>
+      < div  className={classes.markerRow} >
+        <span>{this.state.markerCoords[2][1]} </span>
+        <span> {this.state.markerCoords[2][0]} < /span>
+        < span className={[classes.marker, classes.marker2].join(" ")}
+          id = "marker-2"
+          data-x = {this.state.markerCoords[2][1]}
+          data-y = {this.state.markerCoords[2][0]}
+          draggable = "true"
+          onDragStart = {drag} >
+        < /span>
+      < /div>
 
-   < div  className={classes.markerRow} >
-   <span>{this.state.markerCoords[3][1]} </span>
-   <span> {this.state.markerCoords[3][0]} < /span>
-   < span className={[classes.marker, classes.marker3].join(" ")}
-   id = "marker-3"
-   data-x = {this.state.markerCoords[3][1]}
-   data-y = {this.state.markerCoords[3][0]}
-   draggable = "true"
-   onDragStart = {drag} > < /span>
-   < /div>
+      < div  className={classes.markerRow} >
+        <span>{this.state.markerCoords[3][1]} </span>
+        <span> {this.state.markerCoords[3][0]} < /span>
+        < span className={[classes.marker, classes.marker3].join(" ")}
+          id = "marker-3"
+          data-x = {this.state.markerCoords[3][1]}
+          data-y = {this.state.markerCoords[3][0]}
+          draggable = "true"
+          onDragStart = {drag} >
+        < /span>
+      < /div>
 
-   < /div>
-  );
- }
+      < /div>
+    );
+  }
 }
 
 const colors= ["#66FA93", "#FA6693","#6693FA", "#FA66FA"];
@@ -85,9 +86,9 @@ const randomNum = () => Math.floor(Math.random() * 10 + 1);
 
 // A function that creates an array of n elements of (x, y) coordinates.
 const randomDataSet = () => {
- return Array.apply(null, {
-  length: numDataPoints
- }).map(() => [randomNum(), randomNum()]);
+  return Array.apply(null, {
+    length: numDataPoints
+  }).map(() => [randomNum(), randomNum()]);
 }
 
 export default MarkerWindowLeft
--- src/components/Grid/SquareGrid.js
+++ src/components/Grid/SquareGrid.js
@@ -8,134 +8,134 @@ var isDisabled = false;
 var output = true;
 
 const scale = (x) => {
- return 3 * x;
+  return 3 * x;
 };
 
 const scaleInput = (x) => {
- return 30 * x;
+  return 30 * x;
 };
 
 class SquareGrid extends React.Component {
   constructor(props) {
-     super(props);
-     this.state = {
-        isDisabled: "false",
-        disableAttr : {"disabled":"true"}
-     }
+    super(props);
+    this.state = {
+      isDisabled: "false",
+      disableAttr : {"disabled":"true"}
+    }
   }
   renderSquares(){
-   return (xcoord, index) => {
-    return this.props.xDimensionArrProp.map(this.twoDimensionSquares(xcoord));
-   };
+    return (xcoord, index) => {
+      return this.props.xDimensionArrProp.map(this.twoDimensionSquares(xcoord));
+    };
   };
 
-   twoDimensionSquares(xcoords){
-   return (ycoords, index) => {
-    const squareProps = {
-     x: scale(xcoords),
-     y: scale(ycoords),
-     key: index,
-     width: "30",
-     height: "30",
-     stroke: "black",
-     fill: "transparent",
-     strokeWidth: "1"
-    };
-    return <rect onDrop = {this.drop}
-    onDragOver = {this.allowDrop} {...squareProps}/>
-   }
+  twoDimensionSquares(xcoords){
+    return (ycoords, index) => {
+      const squareProps = {
+        x: scale(xcoords),
+        y: scale(ycoords),
+        key: index,
+        width: "30",
+        height: "30",
+        stroke: "black",
+        fill: "transparent",
+        strokeWidth: "1"
+      };
+      return <rect onDrop = {this.drop}
+      onDragOver = {this.allowDrop} {...squareProps}/>
+    }
   }
 
-   renderTextElements(direction){
-   return (coordinate, index) => {
-    var xVal, yVal;
-    if (direction == "horizontal") {
-     xVal = scale(coordinate) + 10;
-     yVal = 15;
-    } else {
-     xVal = 10;
-     yVal = scale(coordinate) + 15;
-    }
-    const textProps = {
-     x: xVal,
-     y: yVal,
-     key: index,
-     fill: "black"
-    };
-    return <text {...textProps
-    } > {
-     coordinate/10
-    } < /text>;
+  renderTextElements(direction){
+    return (coordinate, index) => {
+      var xVal, yVal;
+      if (direction == "horizontal") {
+        xVal = scale(coordinate) + 10;
+        yVal = 15;
+      } else {
+        xVal = 10;
+        yVal = scale(coordinate) + 15;
+      }
+      const textProps = {
+        x: xVal,
+        y: yVal,
+        key: index,
+        fill: "black"
+      };
+      return <text {...textProps
+      } > {
+        coordinate/10
+      } < /text>;
 
-   }
+    }
   }
 
   allowDrop(ev) {
-  ev.preventDefault();
- }
+    ev.preventDefault();
+  }
 
   drop=(ev)=>{
-  ev.preventDefault();
-  var data = ev.dataTransfer.getData("markerId");
-  var backgroundColor = ev.dataTransfer.getData("backgroundColor");
-  var draggedElem = document.getElementById(data);
-  draggedElem.setAttribute("style", "display:none");
-  ev.target.setAttribute("fill", backgroundColor);
-  var x = draggedElem.getAttribute("data-x");
-  var y = draggedElem.getAttribute("data-y");
-  var x1 = ev.target.getAttribute("x");
-  var y1 = ev.target.getAttribute("y");
-  placedMarkerCount++;
-  if (scaleInput(x) != x1 || scaleInput(y) != y1) {
-   output = false;
+    ev.preventDefault();
+    var data = ev.dataTransfer.getData("markerId");
+    var backgroundColor = ev.dataTransfer.getData("backgroundColor");
+    var draggedElem = document.getElementById(data);
+    draggedElem.setAttribute("style", "display:none");
+    ev.target.setAttribute("fill", backgroundColor);
+    var x = draggedElem.getAttribute("data-x");
+    var y = draggedElem.getAttribute("data-y");
+    var x1 = ev.target.getAttribute("x");
+    var y1 = ev.target.getAttribute("y");
+    placedMarkerCount++;
+    if (scaleInput(x) != x1 || scaleInput(y) != y1) {
+      output = false;
+    }
+    this.updateIsDisabled();
   }
-  this.updateIsDisabled();
- }
 
   checkValid() {
-  if (output  == true) {
-   alert("Correct!");
-  } else {
-   alert("incorrect ....Try again..");
+    if (output  == true) {
+      alert("Correct!");
+    } else {
+      alert("incorrect ....Try again..");
+    }
   }
- }
 
- updateIsDisabled() {
- if (placedMarkerCount == this.props.markerCount) {
-     this.state.isDisabled = "false";
-     this.setState({isDisabled: "false",disableAttr : {}});
-   } else {
-  this.state.isDisabled = "true";
-   this.setState({isDisabled: "true",disableAttr : {"disabled":"true"}});
- }
-}
+  updateIsDisabled() {
+    if (placedMarkerCount == this.props.markerCount) {
+      this.state.isDisabled = "false";
+      this.setState({isDisabled: "false",disableAttr : {}});
+    } else {
+      this.state.isDisabled = "true";
+      this.setState({isDisabled: "true",disableAttr : {"disabled":"true"}});
+    }
+  }
 
-render() {
- var that = this;
- console.log(this.state);
- return <div >
- < svg width = "400"
- height = "400" >
- < g >
-  {this.props.xDimensionArrProp.map(this.renderTextElements("horizontal"))}
- < /g>
- < g >
- {this.props.xDimensionArrProp.map(this.renderTextElements("vertical"))}
- < /g>
- < g >
-  {this.props.xDimensionArrProp.map(this.renderSquares())}
- < /g>
- <g>
-<text x="340" y="15" fill="black"> X </text>
-<text x="10" y="345" fill="black"> Y </text>
-</g>
-< /svg>
-<br/>
- < button className = "btn"
- onClick = {that.checkValid}
- id = "check-btn"  {...this.state.disableAttr} >
- Check < /button> < /div>
-}
+  render() {
+    var that = this;
+    console.log(this.state);
+    return <div >
+    < svg width = "400"
+    height = "400" >
+    < g >
+    {this.props.xDimensionArrProp.map(this.renderTextElements("horizontal"))}
+    < /g>
+    < g >
+    {this.props.xDimensionArrProp.map(this.renderTextElements("vertical"))}
+    < /g>
+    < g >
+    {this.props.xDimensionArrProp.map(this.renderSquares())}
+    < /g>
+    <g>
+    <text x="340" y="15" fill="black"> X </text>
+    <text x="10" y="345" fill="black"> Y </text>
+    </g>
+    < /svg>
+    <br/>
+    < button className = "btn"
+    onClick = {that.checkValid}
+    id = "check-btn"  {...this.state.disableAttr} >
+    Check < /button> < /div>
+  }
 }
 
 export default SquareGrid
